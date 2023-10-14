54ab517fea13982473b0173b3f350d94e14e7205
莫猫猫的狗一只啦 <2334579301@qq.com>
2023-10-14 15:13:47 +0800

    修改了一下

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,42 +1,22 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
-      label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+    <div class="stars">
+      <div v-for="(item, index) in starsCount" :key="index" class="star" ref="star"></div>
+    </div>
+    <div class="a">
+      <div class="b"></div>
+      <div class="c">
+        <div class="d" ref="loginForm" :model="loginForm">
+          <h1>北交疯人院</h1>
+          <h2>Login</h2>
+          <input type="text" class="e" placeholder="USER NAME" v-model="loginForm.username">
+          <input type="password" class="e" placeholder="PASSWORD" v-model="loginForm.password">
+          <el-button class="g" type="primary" @click.native.prevent="handleLogin">Login</el-button>
+          <el-button type="text" style="color: black; font-weight: 700;" @click.native.prevent="handleSign">sign
+            in</el-button>
+        </div>
       </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text"
-          tabindex="1" auto-complete="on" />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType"
-          placeholder="Password" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
-        @click.native.prevent="handleLogin">Login</el-button>
-      <el-button type="text" style="width:5%;margin-bottom:5px;margin-top: -40px;margin-left: 10px"
-        @click.native.prevent="handleSign">sign in</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
-    </el-form>
+    </div>
   </div>
 </template>
 
@@ -69,8 +49,23 @@ export default {
       },
       loading: false,
       passwordType: 'password',
-      redirect: undefined
+      redirect: undefined,
+      starsCount: 2000, //星星数量
+      distance: 600 //间距
     }
+  },
+  mounted () {
+    let starArr = this.$refs.star
+    starArr.forEach(item => {
+      let speed = 0.2 + (Math.random() * 1);
+      let thisDistance = this.distance + (Math.random() * 300);
+      item.style.transformOrigin = `0 0 ${thisDistance}px`;
+      item.style.transform = `
+        translate3d(0,0,-${thisDistance}px)
+        rotateY(${Math.random() * 360}deg)
+        rotateX(${Math.random() * -50}deg)
+        scale(${speed},${speed})`;
+    })
   },
   watch: {
     $route: {
@@ -94,21 +89,12 @@ export default {
       })
     },
     handleLogin () {
-      this.$refs.loginForm.validate(valid => {
-        if (valid)
-        {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else
-        {
-          console.log('error submit!!')
-          return false
-        }
+      this.loading = true
+      this.$store.dispatch('user/login', this.loginForm).then(() => {
+        this.$router.push({ path: this.redirect || '/' })
+        this.loading = false
+      }).catch(() => {
+        this.loading = false
       })
     },
     handleSign () {
@@ -164,26 +150,65 @@ $cursor: #fff;
     color: #454545;
   }
 }
-</style>
 
-<style lang="scss" scoped>
 $bg: #2d3a4b;
 $dark_gray: #889aa4;
 $light_gray: #eee;
 
+@keyframes rotate {
+  0% {
+    transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(0);
+  }
+
+  100% {
+    transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(-360deg);
+  }
+}
+
+.stars {
+  transform: perspective(500px);
+  transform-style: preserve-3d;
+  position: absolute;
+  perspective-origin: 50% 100%;
+  // left: 50%;
+  animation: rotate 90s infinite linear;
+  bottom: 0;
+}
+
+.star {
+  width: 2px;
+  height: 2px;
+  background: #f7f7b6;
+  position: absolute;
+  left: 0;
+  top: 0;
+  backface-visibility: hidden;
+}
+
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  height: 100%;
+  background-image: linear-gradient(to bottom, #000000, #3cadeb);
+  display: flex;
   overflow: hidden;
+  justify-content: center;
+  position: absolute;
 
   .login-form {
     position: relative;
     width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
+    height: 350px;
+    padding: 70px 35px 0;
+    background-color: transparent;
+    border: 2px solid #f7f7b6;
+    backdrop-filter: blur(8px);
+    margin: auto;
+    margin-top: 100px;
     overflow: hidden;
+    justify-content: center;
+    align-items: center;
   }
 
   .tips {
@@ -227,5 +252,73 @@ $light_gray: #eee;
     cursor: pointer;
     user-select: none;
   }
+}
+
+.a {
+  position: relative;
+  top: 100px;
+  width: 1100px;
+  height: 550px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, .8);
+  display: flex;
+}
+
+.b {
+  width: 800px;
+  height: 550px;
+  background-image: url(../../assets/background/H.jpg);
+  /* 让图片适应大小 */
+  background-size: cover;
+  font: 900 50px '';
+}
+
+.c {
+  width: 300px;
+  height: 550px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.d {
+  width: 250px;
+  height: 500px;
+}
+
+.d h1 {
+  font: 900 30px '';
+}
+
+.e {
+  width: 230px;
+  margin: 20px 0;
+  outline: none;
+  border: 0;
+  padding: 10px;
+  border-bottom: 3px solid rgb(80, 80, 170);
+  font: 900 16px '';
+  line-height: 30px;
+}
+
+.f {
+  float: right;
+  margin: 10px 0;
+}
+
+.g {
+  position: absolute;
+  margin: 20px;
+  bottom: 85px;
+  display: block;
+  width: 200px;
+  height: 60px;
+  font: 900 30px '';
+  text-decoration: none;
+  line-height: 30px;
+  border-radius: 30px;
+  background-image: linear-gradient(to left,
+      #9c88ff, #3cadeb);
+  text-align: center;
 }
 </style>
